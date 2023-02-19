a9b56d28e8724568f7fdb63df2ea64c858204b8f
Safin Ahmed <safin.ahmed2000@gmail.com>
2023-02-19 13:34:50 +0600

    Changed the server url

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:4000", {
+  const response = await fetch("https://codex-demo-3hm9.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
